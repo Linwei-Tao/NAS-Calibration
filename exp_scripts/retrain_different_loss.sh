91ca09f3b1a671b623c49900867067b253bddf4c
Linwei Tao <dylan.tao94@gmail.com>
2022-09-14 00:20:58 +1000

    scripts

--- exp_scripts/retrain_different_loss.sh
+++ exp_scripts/retrain_different_loss.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ../
-CUDA_VISIBLE_DEVICES=0 python3 train.py --drop_path_prob=0 --weight_decay=5e-4 --epochs=350 --scheduler=focal --batch_size=128 --save=retrain_softece --arch=DARTS_SOFTECE &
-CUDA_VISIBLE_DEVICES=1 python3 train.py --drop_path_prob=0 --weight_decay=5e-4 --epochs=350 --scheduler=focal --batch_size=128 --save=retrain_mmce --arch=DARTS_MMCE
+#CUDA_VISIBLE_DEVICES=0 python3 train.py --drop_path_prob=0 --weight_decay=5e-4 --epochs=350 --scheduler=focal --batch_size=128 --save=retrain_softece --arch=DARTS_SOFTECE &
+CUDA_VISIBLE_DEVICES=1 python3 train.py --drop_path_prob=0 --weight_decay=5e-4 --epochs=350 --scheduler=focal --batch_size=128 --arch=DARTS_SOFTECE10 --save=retrain --criterion=softece
 
 
 
